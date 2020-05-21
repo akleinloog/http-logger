0b967e7d3accd59419fcc2d48258453256e0dd71
Arnoud Kleinloog <akleinloog@gmail.com>
2020-05-21 04:15:55 +0200

    First step HTTP Server

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # http-logger
-A simple HTTP Logger
+
+A simple HTTP Logger in Go.
+
+
+## Run a local instance
+
+After cloning the repository, start a fresh local image using
+```
+docker-compose up --build
+```
+
+content-type: application/json
+
+{
+  "requestId": "req-001-kk",
+  "name": "Test Domain"
+}
+
+
+## HTTP GET Listener
+
+Based on 
+https://gobyexample.com/http-servers
+
---
+++ cmd/serve.go
@@ -0,0 +1,91 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Starts the HTTP Logger",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		log.Println("Starting server :80")
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", root)
+		mux.HandleFunc("/hello", hello)
+		mux.HandleFunc("/headers", headers)
+
+		server := &http.Server{
+			Addr:         ":80",
+			Handler:      mux,
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			IdleTimeout:  120 * time.Second,
+		}
+
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Server startup failed")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func root(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "Welcome to the Root...\n")
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
