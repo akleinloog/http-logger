5ffd8e9584a99d81de146401a05f6034bd7c45af
Arnoud Kleinloog <arnoud@kleinloog.ch>
2020-05-25 22:42:29 +0200

    Fixed image labels

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,20 @@
 set -e
 
 SEM_VER=$(git describe --abbrev=0)
-CODE_VER=$(git describe --dirty)
+CODE_VER=$(git describe --dirty="-mod")
 GIT_COMMIT=$(git rev-parse HEAD)
 
 echo "Building container image"
 echo "Version: " $CODE_VER
 echo "Commit: " $GIT_COMMIT
 
-docker build --force-rm --build-arg version=$CODE_VER --build-arg vcs-ref=$GIT_COMMIT -f ./docker/Dockerfile -t akleinloog/http-logger:$CODE_VER .
+docker build --force-rm --build-arg=VERSION=$CODE_VER --build-arg=VCS_REF=$GIT_COMMIT -f ./docker/Dockerfile -t akleinloog/http-logger:$CODE_VER .
+
+#--build-arg=VERSION=$CODE_VER --build-arg=VCS-REF=$GIT_COMMIT
 
 if [ $CODE_VER == $SEM_VER ]
   then
-    echo "Publishing mage as latest version to docker hub"
+    echo "Publishing image as latest version to docker hub"
     docker tag akleinloog/http-logger:$CODE_VER akleinloog/http-logger:latest
     docker push akleinloog/http-logger:$CODE_VER
     docker push akleinloog/http-logger:latest
