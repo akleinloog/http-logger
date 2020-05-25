e77a643a3d57c561fc12bdd7156035ef57cc6857
Arnoud Kleinloog <arnoud@kleinloog.ch>
2020-05-25 22:10:11 +0200

    Image build script

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,24 @@
 
 set -e
 
-echo "Building Container"
-
-SEMVER=$(git describe --abbrev=0)
-CODEVER=$(git describe --dirty="*")
+SEM_VER=$(git describe --abbrev=0)
+CODE_VER=$(git describe --dirty)
 GIT_COMMIT=$(git rev-parse HEAD)
 
-echo "Version: " $CODEVER
+echo "Building container image"
+echo "Version: " $CODE_VER
 echo "Commit: " $GIT_COMMIT
 
-if [ $CODEVER == $SEMVER ]
+docker build --force-rm --build-arg version=$CODE_VER --build-arg vcs-ref=$GIT_COMMIT -f ./docker/Dockerfile -t akleinloog/http-logger:$CODE_VER .
+
+if [ $CODE_VER == $SEM_VER ]
   then
-    echo "Publishing Image"
+    echo "Publishing mage as latest version to docker hub"
+    docker tag akleinloog/http-logger:$CODE_VER akleinloog/http-logger:latest
+    docker push akleinloog/http-logger:$CODE_VER
+    docker push akleinloog/http-logger:latest
   else
-    echo "Not an official release"
+    echo "This is not an official release, the image won't be pushed to docker hub"
 fi
+
+echo "Build completed"
