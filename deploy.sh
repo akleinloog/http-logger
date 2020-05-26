58aed18caf286ba817c603df6fe62917417d2351
Arnoud Kleinloog <arnoud.kleinloog@contractors.roche.com>
2020-05-26 16:39:51 +0200

    simplified script and docker file labelling

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+IMAGE="akleinloog/http-logger"
 SEM_VER=$(git describe --abbrev=0)
 CODE_VER=$(git describe --dirty="-mod")
 GIT_COMMIT=$(git rev-parse HEAD)
@@ -10,14 +11,14 @@ echo "Building container image"
 echo "Version: " $CODE_VER
 echo "Commit: " $GIT_COMMIT
 
-docker build --force-rm --build-arg=VERSION=$CODE_VER --build-arg=VCS_REF=$GIT_COMMIT -f ./docker/Dockerfile -t akleinloog/http-logger:$CODE_VER .
+docker build --force-rm --build-arg=VERSION=$CODE_VER --build-arg=VCS_REF=$GIT_COMMIT -f ./docker/Dockerfile -t $IMAGE:$CODE_VER .
 
 if [ $CODE_VER == $SEM_VER ]
   then
     echo "Publishing image as latest version to docker hub"
-    docker tag akleinloog/http-logger:$CODE_VER akleinloog/http-logger:latest
-    docker push akleinloog/http-logger:$CODE_VER
-    docker push akleinloog/http-logger:latest
+    docker tag $IMAGE:$CODE_VER $IMAGE:latest
+    docker push $IMAGE:$CODE_VER
+    docker push $IMAGE:latest
   else
     echo "This is not an official release, the image won't be pushed to docker hub"
 fi
